44d68af1076c441c327cc6eeb94f876db488317c
RodicaNegura <RodicaElenaNegura@gmail.com>
2022-11-25 15:08:57 +0000

    insert swiper button

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ window.onscroll = () => {
   searchForm.classList.remove('active')
   navbar.classList.remove('active')
 }
+
+var swiper = new Swiper(".home-slider", {
+  centeredSlides: true,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
