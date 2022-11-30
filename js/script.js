a5f8e0d8ef6f2f0411b2b89a63cd02eb36f2bacb
RodicaNegura <RodicaElenaNegura@gmail.com>
2022-11-30 11:42:48 +0000

    blogs section

--- js/script.js
+++ js/script.js
@@ -99,3 +99,28 @@ var swiper = new Swiper(".review-slide", {
     },
   },
 })
+
+var swiper = new Swiper(".blogs-slider", {
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  },
+})
